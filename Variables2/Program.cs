52a3c826dc8bf3a69e71fcb167b50fafe301227a
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-15 11:10:37 +0300

    Proje dosyası ekle.

---
+++ Variables2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi;
+            //sayi = 38;
+            //Console.Write(sayi);
+
+            //int sayi1, sayi2, toplam;
+            //sayi1 = 3;
+            //sayi2 = 5;
+            //toplam = sayi1 + sayi2;
+
+            //Console.Write(toplam);
+
+            //kısa kenarı 5cm, uzun kenarı ise 7cm olan bir dikdörtgenin çevre ve alanını hesaplayın
+
+            //int kisaKenar, uzunKenar, cevre;
+            //kisaKenar = 5;
+            //uzunKenar = 7;
+            //cevre = 2*(kisaKenar + uzunKenar);
+            //Console.Write(cevre);
+
+            int kisaKenar, uzunKenar, alan;
+            kisaKenar = 5;
+            uzunKenar = 7;
+            alan = kisaKenar * uzunKenar;
+            Console.Write(" alan : " + alan);
+
+
+            
+
+
+            Console.Read();
+        }
+    }
+}
